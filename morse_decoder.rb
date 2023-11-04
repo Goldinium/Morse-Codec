d6d67ad21c0c0cd67451609e669faeeba3372d1e
Goldinium <badrbenslama8@gmail.com>
2023-11-04 11:34:28 +0100

    fix linter errs

---
+++ morse_decoder.rb
@@ -0,0 +1,27 @@
+class MorseCodeDecoder
+  MORSE = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+    '--..' => 'Z'
+  }.freeze
+
+  def char_decoder(crypt)
+    MORSE[crypt] || ''
+  end
+
+  def letter_decoder(letter)
+    letter.split.map { |char| char_decoder(char) }.join
+  end
+
+  def decode(message)
+    message.split('   ').map { |letter| letter_decoder(letter) }.join(' ')
+  end
+end
+
+decoder = MorseCodeDecoder.new
+# rubocop:disable Layout/LineLength
+puts decoder.decode('.... - - .--. ... ---... -..-. -..-. .-- .-- .-- .-.-.- -.-- --- ..- - ..- -... . .-.-.- -.-. --- -- -..-. .-- .- - -.-. .... ..--.. ...- -...- -.. --.- .-- ....- .-- ----. .-- --. -..- -.-. --.- .-... .--. .--. -...- -.-- --. ..- .. -.-. -- .-.. .--- .- ...-- .--- ...- -... --. .-- ...-- -..')
+# rubocop:enable Layout/LineLength
